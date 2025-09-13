c73dc86a2a61d8e9b55b36ba2d422d5373d604d3
Matias Ballesteros Quintana <matias.quintanaballesteros@gmail.com>
2025-09-13 14:41:59 -0300

    Changes frmPrincipal
    Cuando compilas el programa ejecuta la frmPrincipal

--- TPWINFORM_Equipo-17B/TPWINFORM_Equipo-17B/Program.cs
+++ TPWINFORM_Equipo-17B/TPWINFORM_Equipo-17B/Program.cs
@@ -16,7 +16,7 @@ namespace TPWINFORM_Equipo_17B
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmPrincipal());
         }
     }
 }
